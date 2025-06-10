5813314a32f5307ad41ec66d262b855ee84b9304
roeiben@bgufbm.onmicrosoft.com <roeiben@bgufbm.onmicrosoft.com>
2025-06-10 20:11:50 +0300

    participant file

---
+++ G5/Forms/NewParticiapant.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace G5.Forms
+{
+    public partial class NewParticiapant : Form
+    {
+        public NewParticiapant()
+        {
+            InitializeComponent();
+        }
+
+        private void ParticipantId_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ParticipantId_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ParticipantGenderCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AgeGroupCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PatricipantMedicalNotes_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CancelParticipantCreation_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveParticipantCreation_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
