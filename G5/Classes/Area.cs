56af565dc47239cfddb4443ca2fa51affee9916b
hadad4@bgufbm.onmicrosoft.com <hadad4@bgufbm.onmicrosoft.com>
2025-06-12 16:22:21 +0300

    MayaStriko

---
+++ G5/Classes/Area.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G5
+{
+    public class Area
+    {
+        // Properties
+        public string AreaID { get; set; }
+        public string AreaName { get; set; }
+        public double Size { get; set; }
+        public bool IsAvailable { get; set; }
+
+        // Constructor
+        public Area(string areaID, string areaName, double size, bool isAvailable)
+        {
+            AreaID = areaID;
+            AreaName = areaName;
+            Size = size;
+            IsAvailable = isAvailable;
+        }
+
+        // Optional: override ToString for easy display
+        public override string ToString()
+        {
+            return $"Area ID: {AreaID}, Name: {AreaName}, Size: {Size} sqm, Available: {IsAvailable}";
+        }
+    }
+}
