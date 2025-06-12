34aee5877eda44bc00531b8f4ba6634f2011d0a1
mayastr@bgufbm.onmicrosoft.com <mayastr@bgufbm.onmicrosoft.com>
2025-06-12 12:23:40 +0300

    קומיט של מאיה

--- G5/Forms/NewParticiapant.Designer.cs
+++ G5/Forms/NewParticiapant.Designer.cs
@@ -58,9 +58,10 @@
             // 
             // SaveParticipantCreation
             // 
-            this.SaveParticipantCreation.Location = new System.Drawing.Point(28, 368);
+            this.SaveParticipantCreation.Location = new System.Drawing.Point(19, 236);
+            this.SaveParticipantCreation.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.SaveParticipantCreation.Name = "SaveParticipantCreation";
-            this.SaveParticipantCreation.Size = new System.Drawing.Size(122, 54);
+            this.SaveParticipantCreation.Size = new System.Drawing.Size(81, 35);
             this.SaveParticipantCreation.TabIndex = 0;
             this.SaveParticipantCreation.Text = "שמור";
             this.SaveParticipantCreation.UseVisualStyleBackColor = true;
@@ -68,9 +69,10 @@
             // 
             // CancelParticipantCreation
             // 
-            this.CancelParticipantCreation.Location = new System.Drawing.Point(176, 368);
+            this.CancelParticipantCreation.Location = new System.Drawing.Point(117, 236);
+            this.CancelParticipantCreation.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.CancelParticipantCreation.Name = "CancelParticipantCreation";
-            this.CancelParticipantCreation.Size = new System.Drawing.Size(173, 54);
+            this.CancelParticipantCreation.Size = new System.Drawing.Size(115, 35);
             this.CancelParticipantCreation.TabIndex = 1;
             this.CancelParticipantCreation.Text = "בטל ";
             this.CancelParticipantCreation.UseVisualStyleBackColor = true;
@@ -79,77 +81,86 @@
             // ParticipantIdLable
             // 
             this.ParticipantIdLable.AutoSize = true;
-            this.ParticipantIdLable.Location = new System.Drawing.Point(651, 85);
+            this.ParticipantIdLable.Location = new System.Drawing.Point(434, 54);
+            this.ParticipantIdLable.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.ParticipantIdLable.Name = "ParticipantIdLable";
-            this.ParticipantIdLable.Size = new System.Drawing.Size(116, 25);
+            this.ParticipantIdLable.Size = new System.Drawing.Size(76, 16);
             this.ParticipantIdLable.TabIndex = 2;
             this.ParticipantIdLable.Text = "תעודת זהות";
             // 
             // FirstNameLable
             // 
             this.FirstNameLable.AutoSize = true;
-            this.FirstNameLable.Location = new System.Drawing.Point(651, 131);
+            this.FirstNameLable.Location = new System.Drawing.Point(434, 84);
+            this.FirstNameLable.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.FirstNameLable.Name = "FirstNameLable";
-            this.FirstNameLable.Size = new System.Drawing.Size(88, 25);
+            this.FirstNameLable.Size = new System.Drawing.Size(54, 16);
             this.FirstNameLable.TabIndex = 3;
             this.FirstNameLable.Text = "שם פרטי";
             // 
             // ParticipantLastName
             // 
             this.ParticipantLastName.AutoSize = true;
-            this.ParticipantLastName.Location = new System.Drawing.Point(651, 178);
+            this.ParticipantLastName.Location = new System.Drawing.Point(434, 114);
+            this.ParticipantLastName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.ParticipantLastName.Name = "ParticipantLastName";
-            this.ParticipantLastName.Size = new System.Drawing.Size(110, 25);
+            this.ParticipantLastName.Size = new System.Drawing.Size(67, 16);
             this.ParticipantLastName.TabIndex = 4;
             this.ParticipantLastName.Text = "שם משפחה";
             // 
             // ParticipantId
             // 
-            this.ParticipantId.Location = new System.Drawing.Point(433, 85);
+            this.ParticipantId.Location = new System.Drawing.Point(289, 54);
+            this.ParticipantId.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ParticipantId.Name = "ParticipantId";
-            this.ParticipantId.Size = new System.Drawing.Size(184, 31);
+            this.ParticipantId.Size = new System.Drawing.Size(124, 22);
             this.ParticipantId.TabIndex = 5;
             this.ParticipantId.Click += new System.EventHandler(this.ParticipantId_Click);
             this.ParticipantId.TextChanged += new System.EventHandler(this.ParticipantId_TextChanged);
             // 
             // ParticipantAdress
             // 
-            this.ParticipantAdress.Location = new System.Drawing.Point(433, 326);
+            this.ParticipantAdress.Location = new System.Drawing.Point(289, 209);
+            this.ParticipantAdress.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ParticipantAdress.Name = "ParticipantAdress";
-            this.ParticipantAdress.Size = new System.Drawing.Size(183, 31);
+            this.ParticipantAdress.Size = new System.Drawing.Size(123, 22);
             this.ParticipantAdress.TabIndex = 6;
             this.ParticipantAdress.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(433, 178);
+            this.textBox3.Location = new System.Drawing.Point(289, 114);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(184, 31);
+            this.textBox3.Size = new System.Drawing.Size(124, 22);
             this.textBox3.TabIndex = 7;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // ParticipantFirstName
             // 
-            this.ParticipantFirstName.Location = new System.Drawing.Point(433, 131);
+            this.ParticipantFirstName.Location = new System.Drawing.Point(289, 84);
+            this.ParticipantFirstName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ParticipantFirstName.Name = "ParticipantFirstName";
-            this.ParticipantFirstName.Size = new System.Drawing.Size(184, 31);
+            this.ParticipantFirstName.Size = new System.Drawing.Size(124, 22);
             this.ParticipantFirstName.TabIndex = 8;
             this.ParticipantFirstName.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // ParticipantBirthDate
             // 
-            this.ParticipantBirthDate.Location = new System.Drawing.Point(433, 226);
+            this.ParticipantBirthDate.Location = new System.Drawing.Point(289, 145);
+            this.ParticipantBirthDate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ParticipantBirthDate.Name = "ParticipantBirthDate";
-            this.ParticipantBirthDate.Size = new System.Drawing.Size(183, 31);
+            this.ParticipantBirthDate.Size = new System.Drawing.Size(123, 22);
             this.ParticipantBirthDate.TabIndex = 10;
             this.ParticipantBirthDate.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(652, 231);
+            this.label1.Location = new System.Drawing.Point(435, 148);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(115, 25);
+            this.label1.Size = new System.Drawing.Size(76, 16);
             this.label1.TabIndex = 11;
             this.label1.Text = "תאריך לידה";
             this.label1.Click += new System.EventHandler(this.ParticipantId_TextChanged);
@@ -157,18 +168,20 @@
             // ParticipantGenderCombo
             // 
             this.ParticipantGenderCombo.FormattingEnabled = true;
-            this.ParticipantGenderCombo.Location = new System.Drawing.Point(432, 275);
+            this.ParticipantGenderCombo.Location = new System.Drawing.Point(288, 176);
+            this.ParticipantGenderCombo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ParticipantGenderCombo.Name = "ParticipantGenderCombo";
-            this.ParticipantGenderCombo.Size = new System.Drawing.Size(184, 33);
+            this.ParticipantGenderCombo.Size = new System.Drawing.Size(124, 24);
             this.ParticipantGenderCombo.TabIndex = 12;
             this.ParticipantGenderCombo.SelectedIndexChanged += new System.EventHandler(this.ParticipantGenderCombo_SelectedIndexChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(652, 278);
+            this.label2.Location = new System.Drawing.Point(435, 178);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 25);
+            this.label2.Size = new System.Drawing.Size(36, 16);
             this.label2.TabIndex = 13;
             this.label2.Text = "מגדר";
             this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -177,18 +190,20 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(651, 383);
+            this.label3.Location = new System.Drawing.Point(434, 245);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(101, 25);
+            this.label3.Size = new System.Drawing.Size(65, 16);
             this.label3.TabIndex = 14;
             this.label3.Text = "קבוצת גיל";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(652, 329);
+            this.label4.Location = new System.Drawing.Point(435, 211);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(68, 25);
+            this.label4.Size = new System.Drawing.Size(45, 16);
             this.label4.TabIndex = 15;
             this.label4.Text = "כתובת";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -196,9 +211,10 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(243, 293);
+            this.label5.Location = new System.Drawing.Point(162, 188);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(143, 25);
+            this.label5.Size = new System.Drawing.Size(92, 16);
             this.label5.TabIndex = 16;
             this.label5.Text = "דמי חבר שולמו";
             this.label5.Click += new System.EventHandler(this.label5_Click);
@@ -206,9 +222,10 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(243, 88);
+            this.label6.Location = new System.Drawing.Point(162, 56);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(154, 25);
+            this.label6.Size = new System.Drawing.Size(98, 16);
             this.label6.TabIndex = 17;
             this.label6.Text = "תאריך הצטפרות";
             this.label6.Click += new System.EventHandler(this.label6_Click);
@@ -216,9 +233,10 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(218, 145);
+            this.label7.Location = new System.Drawing.Point(145, 93);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(213, 25);
+            this.label7.Size = new System.Drawing.Size(131, 16);
             this.label7.TabIndex = 18;
             this.label7.Text = "מספר טלפון - איש קשר";
             this.label7.Click += new System.EventHandler(this.label7_Click);
@@ -226,75 +244,83 @@
             // AgeGroupCombo
             // 
             this.AgeGroupCombo.FormattingEnabled = true;
-            this.AgeGroupCombo.Location = new System.Drawing.Point(433, 375);
+            this.AgeGroupCombo.Location = new System.Drawing.Point(289, 240);
+            this.AgeGroupCombo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.AgeGroupCombo.Name = "AgeGroupCombo";
-            this.AgeGroupCombo.Size = new System.Drawing.Size(183, 33);
+            this.AgeGroupCombo.Size = new System.Drawing.Size(123, 24);
             this.AgeGroupCombo.TabIndex = 20;
             this.AgeGroupCombo.SelectedIndexChanged += new System.EventHandler(this.AgeGroupCombo_SelectedIndexChanged);
             // 
             // ParticipantPaymentStatusCheckBox
             // 
             this.ParticipantPaymentStatusCheckBox.AutoSize = true;
-            this.ParticipantPaymentStatusCheckBox.Location = new System.Drawing.Point(122, 281);
+            this.ParticipantPaymentStatusCheckBox.Location = new System.Drawing.Point(81, 180);
+            this.ParticipantPaymentStatusCheckBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ParticipantPaymentStatusCheckBox.Name = "ParticipantPaymentStatusCheckBox";
-            this.ParticipantPaymentStatusCheckBox.Size = new System.Drawing.Size(28, 27);
+            this.ParticipantPaymentStatusCheckBox.Size = new System.Drawing.Size(18, 17);
             this.ParticipantPaymentStatusCheckBox.TabIndex = 21;
             this.ParticipantPaymentStatusCheckBox.UseVisualStyleBackColor = true;
             // 
             // ParticipantJoinDATE
             // 
-            this.ParticipantJoinDATE.Location = new System.Drawing.Point(12, 88);
+            this.ParticipantJoinDATE.Location = new System.Drawing.Point(8, 56);
+            this.ParticipantJoinDATE.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ParticipantJoinDATE.Name = "ParticipantJoinDATE";
-            this.ParticipantJoinDATE.Size = new System.Drawing.Size(200, 31);
+            this.ParticipantJoinDATE.Size = new System.Drawing.Size(135, 22);
             this.ParticipantJoinDATE.TabIndex = 22;
             this.ParticipantJoinDATE.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 142);
+            this.textBox1.Location = new System.Drawing.Point(8, 91);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(200, 31);
+            this.textBox1.Size = new System.Drawing.Size(135, 22);
             this.textBox1.TabIndex = 23;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(171, 203);
+            this.label8.Location = new System.Drawing.Point(114, 130);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(255, 25);
+            this.label8.Size = new System.Drawing.Size(161, 16);
             this.label8.TabIndex = 24;
             this.label8.Text = "הערות רפואיות - אם קיימות";
             // 
             // PatricipantMedicalNotes
             // 
-            this.PatricipantMedicalNotes.Location = new System.Drawing.Point(12, 197);
+            this.PatricipantMedicalNotes.Location = new System.Drawing.Point(8, 126);
+            this.PatricipantMedicalNotes.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.PatricipantMedicalNotes.Name = "PatricipantMedicalNotes";
-            this.PatricipantMedicalNotes.Size = new System.Drawing.Size(153, 31);
+            this.PatricipantMedicalNotes.Size = new System.Drawing.Size(103, 22);
             this.PatricipantMedicalNotes.TabIndex = 25;
             this.PatricipantMedicalNotes.TextChanged += new System.EventHandler(this.PatricipantMedicalNotes_TextChanged);
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(311, 244);
+            this.label9.Location = new System.Drawing.Point(207, 156);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(86, 25);
+            this.label9.Size = new System.Drawing.Size(54, 16);
             this.label9.TabIndex = 26;
             this.label9.Text = "בית ספר";
             // 
             // ParticipantSchool
             // 
-            this.ParticipantSchool.Location = new System.Drawing.Point(29, 244);
+            this.ParticipantSchool.Location = new System.Drawing.Point(19, 156);
+            this.ParticipantSchool.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ParticipantSchool.Name = "ParticipantSchool";
-            this.ParticipantSchool.Size = new System.Drawing.Size(136, 31);
+            this.ParticipantSchool.Size = new System.Drawing.Size(92, 22);
             this.ParticipantSchool.TabIndex = 27;
             this.ParticipantSchool.TextChanged += new System.EventHandler(this.ParticipantSchool_TextChanged);
             // 
             // NewParticiapant
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(533, 288);
             this.Controls.Add(this.ParticipantSchool);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.PatricipantMedicalNotes);
@@ -321,6 +347,7 @@
             this.Controls.Add(this.ParticipantIdLable);
             this.Controls.Add(this.CancelParticipantCreation);
             this.Controls.Add(this.SaveParticipantCreation);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "NewParticiapant";
             this.Text = "NewParticiapant";
             this.ResumeLayout(false);
