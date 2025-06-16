cb96ea8992226578f453ef0c4d51ca7d8a35c533
roeiben@bgufbm.onmicrosoft.com <roeiben@bgufbm.onmicrosoft.com>
2025-06-16 22:31:18 +0300

    connection to activities

--- G5/Forms/MainPageForm.Designer.cs
+++ G5/Forms/MainPageForm.Designer.cs
@@ -30,7 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainPageForm));
             this.MembersButton = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.ActivityButton = new System.Windows.Forms.Button();
             this.ParticipantsButton = new System.Windows.Forms.Button();
             this.MessagesButton = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
@@ -41,35 +41,34 @@
             // 
             this.MembersButton.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.MembersButton.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.MembersButton.Location = new System.Drawing.Point(272, 194);
-            this.MembersButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MembersButton.Location = new System.Drawing.Point(408, 303);
             this.MembersButton.Name = "MembersButton";
-            this.MembersButton.Size = new System.Drawing.Size(131, 43);
+            this.MembersButton.Size = new System.Drawing.Size(196, 67);
             this.MembersButton.TabIndex = 0;
             this.MembersButton.Text = "בוגרים";
             this.MembersButton.UseVisualStyleBackColor = true;
             this.MembersButton.Click += new System.EventHandler(this.MembersButton_Click);
             // 
-            // button2
+            // ActivityButton
             // 
-            this.button2.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.button2.Location = new System.Drawing.Point(419, 194);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(138, 43);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.ActivityButton.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ActivityButton.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.ActivityButton.Location = new System.Drawing.Point(628, 303);
+            this.ActivityButton.Name = "ActivityButton";
+            this.ActivityButton.Size = new System.Drawing.Size(207, 67);
+            this.ActivityButton.TabIndex = 1;
+            this.ActivityButton.Text = "פעולות";
+            this.ActivityButton.UseVisualStyleBackColor = true;
+//            this.ActivityButton.ContextMenuStripChanged += new System.EventHandler(this.ActivitiesButton_Click);
+            this.ActivityButton.Click += new System.EventHandler(this.ActivityButton_Click);
             // 
             // ParticipantsButton
             // 
             this.ParticipantsButton.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ParticipantsButton.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.ParticipantsButton.Location = new System.Drawing.Point(136, 194);
-            this.ParticipantsButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ParticipantsButton.Location = new System.Drawing.Point(204, 303);
             this.ParticipantsButton.Name = "ParticipantsButton";
-            this.ParticipantsButton.Size = new System.Drawing.Size(120, 43);
+            this.ParticipantsButton.Size = new System.Drawing.Size(180, 67);
             this.ParticipantsButton.TabIndex = 2;
             this.ParticipantsButton.Text = "חניכים";
             this.ParticipantsButton.UseVisualStyleBackColor = true;
@@ -79,10 +78,9 @@
             // 
             this.MessagesButton.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.MessagesButton.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.MessagesButton.Location = new System.Drawing.Point(583, 192);
-            this.MessagesButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MessagesButton.Location = new System.Drawing.Point(874, 300);
             this.MessagesButton.Name = "MessagesButton";
-            this.MessagesButton.Size = new System.Drawing.Size(130, 45);
+            this.MessagesButton.Size = new System.Drawing.Size(195, 70);
             this.MessagesButton.TabIndex = 3;
             this.MessagesButton.Text = "הודעות";
             this.MessagesButton.UseVisualStyleBackColor = true;
@@ -90,24 +88,24 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(18, 19);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(127, 101);
+            this.pictureBox1.Size = new System.Drawing.Size(190, 158);
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
             // MainPageForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LemonChiffon;
-            this.ClientSize = new System.Drawing.Size(882, 503);
+            this.ClientSize = new System.Drawing.Size(1323, 786);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.MessagesButton);
             this.Controls.Add(this.ParticipantsButton);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ActivityButton);
             this.Controls.Add(this.MembersButton);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MainPageForm";
             this.Text = "MainPageForm";
             this.Load += new System.EventHandler(this.MainPageForm_Load);
@@ -119,7 +117,7 @@
         #endregion
 
         private System.Windows.Forms.Button MembersButton;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button ActivityButton;
         private System.Windows.Forms.Button ParticipantsButton;
         private System.Windows.Forms.Button MessagesButton;
         private System.Windows.Forms.PictureBox pictureBox1;
