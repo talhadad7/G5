67d3faa14fc72804fa52f530964ab14a698a659b
mayastr@bgufbm.onmicrosoft.com <mayastr@bgufbm.onmicrosoft.com>
2025-06-16 16:33:19 +0300

    עוד קצת עיצובים

--- G5/Forms/DeleteParticipant.Designer.cs
+++ G5/Forms/DeleteParticipant.Designer.cs
@@ -28,25 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DeleteParticipant));
             this.label1 = new System.Windows.Forms.Label();
             this.DeleteButton = new System.Windows.Forms.Button();
             this.IDTXTBox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(357, 25);
+            this.label1.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label1.Location = new System.Drawing.Point(332, 63);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.Size = new System.Drawing.Size(181, 34);
             this.label1.TabIndex = 0;
             this.label1.Text = "מחיקת חניך";
             // 
             // DeleteButton
             // 
-            this.DeleteButton.Location = new System.Drawing.Point(499, 108);
+            this.DeleteButton.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteButton.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.DeleteButton.Location = new System.Drawing.Point(242, 207);
+            this.DeleteButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.DeleteButton.Name = "DeleteButton";
-            this.DeleteButton.Size = new System.Drawing.Size(167, 68);
+            this.DeleteButton.Size = new System.Drawing.Size(148, 54);
             this.DeleteButton.TabIndex = 1;
             this.DeleteButton.Text = "מחיקה";
             this.DeleteButton.UseVisualStyleBackColor = true;
@@ -54,24 +62,41 @@
             // 
             // IDTXTBox
             // 
-            this.IDTXTBox.Location = new System.Drawing.Point(97, 129);
+            this.IDTXTBox.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IDTXTBox.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.IDTXTBox.Location = new System.Drawing.Point(418, 221);
+            this.IDTXTBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.IDTXTBox.Name = "IDTXTBox";
-            this.IDTXTBox.Size = new System.Drawing.Size(201, 26);
+            this.IDTXTBox.Size = new System.Drawing.Size(234, 28);
             this.IDTXTBox.TabIndex = 2;
             this.IDTXTBox.Text = "נא להזין ת\"ז";
+            this.IDTXTBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.IDTXTBox.TextChanged += new System.EventHandler(this.IDTXTBox_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(127, 101);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
             // 
             // DeleteParticipant
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(882, 503);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.IDTXTBox);
             this.Controls.Add(this.DeleteButton);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "DeleteParticipant";
             this.Text = "DeleteParticipant";
             this.Load += new System.EventHandler(this.DeleteParticipant_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,5 +107,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button DeleteButton;
         private System.Windows.Forms.TextBox IDTXTBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
